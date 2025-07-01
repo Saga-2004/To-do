c5eacd3f59778f19c0c7dece6086d8dc9ecbcaf9
Sagar <msagars2008@gmail.com>
2025-07-01 14:08:44 +0530

    drag-drop

--- script.js
+++ script.js
@@ -1,49 +1,48 @@
 let draggedCard = null;
-function addTask(coluumnId){
-    const input = document.getElementById(`${coluumnId}-input`);
-    // console.log(input);
-    const  taskText = input.value;
-    // console.log(taskText);
+function addTask(coluumnId) {
+  const input = document.getElementById(`${coluumnId}-input`);
+  // console.log(input);
+  const taskText = input.value;
+  // console.log(taskText);
 
-    if(!taskText){
-        return;
-    }
+  if (!taskText) {
+    return;
+  }
 
-    const taskElement = createtaskElement(taskText);
-    document.getElementById(`${coluumnId}-tasks`).appendChild(taskElement);
-    input.value = ""
-    
-} 
-
-function createtaskElement(taskText){
-    const takeElement = document.createElement("li");
-    takeElement.innerText = taskText;
-    takeElement.classList.add("card")
-    
-    // takeElement.setAttribute("draggable",true) //OR 
-    takeElement.draggable = true;
-
-    takeElement.addEventListener("dragstart", dragStart)
-    takeElement.addEventListener("dragend",dragEnd)
-
-    return takeElement
+  const taskElement = createtaskElement(taskText);
+  document.getElementById(`${coluumnId}-tasks`).appendChild(taskElement);
+  input.value = "";
 }
 
-function dragStart(){
-    this.classList.add("dragging");
-    draggedCard = this;
+function createtaskElement(taskText) {
+  const takeElement = document.createElement("li");
+  takeElement.innerText = taskText;
+  takeElement.classList.add("card");
+
+  // takeElement.setAttribute("draggable",true) //OR
+  takeElement.draggable = true;
+
+  takeElement.addEventListener("dragstart", dragStart);
+  takeElement.addEventListener("dragend", dragEnd);
+
+  return takeElement;
 }
 
-function dragEnd(){
-   this.classList.remove("dragging");
+function dragStart() {
+  this.classList.add("dragging");
+  draggedCard = this;
+}
+
+function dragEnd() {
+  this.classList.remove("dragging");
 }
 
 const columns = document.querySelectorAll(".column > ul.tasks");
-columns.forEach((column)=>{
-    column.addEventListener("dragover",dragOver)
-})
+columns.forEach((column) => {
+  column.addEventListener("dragover", dragOver);
+});
 
-function dragOver(e){
-    e.preventDefault();
-    this.appendChild(draggedCard);  
+function dragOver(e) {
+  e.preventDefault();
+  this.appendChild(draggedCard);
 }
